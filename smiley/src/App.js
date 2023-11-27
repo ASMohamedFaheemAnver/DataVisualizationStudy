dde69d6a241183d692b6167d1f9ec17785bebb29
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2023-11-28 00:15:32 +0530

    First svg in react,

---
+++ smiley/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <svg width="960" height="500">
+        <circle
+          cx="50%"
+          cy="50%"
+          r="200"
+          fill="orange"
+          stroke="black"
+          strokeWidth="5px"
+        />
+        <circle
+          cx="45%"
+          cy="45%"
+          r="20"
+          fill="black"
+          stroke="black"
+          strokeWidth="5px"
+        />
+        <circle
+          cx="55%"
+          cy="45%"
+          r="20"
+          fill="black"
+          stroke="black"
+          strokeWidth="5px"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default App;
